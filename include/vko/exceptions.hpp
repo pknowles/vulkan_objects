65475346aac4c29c06d157c19d0053470da1475d
pknowles <pknowles@users.noreply.github.com>
2025-02-16 22:32:51 -0800

    wip dynamic library integration

---
+++ include/vko/exceptions.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2025 Pyarelal Knowles, MIT License
+// This file was generated from handles.hpp.txt. Do not edit directly.
+#pragma once
+
+#include <exception>
+#include <stdexcept>
+
+class Exception : public std::exception {
+public:
+    template<class Str>
+    Exception(Str&& message)
+        : m_what(std::forward<Str>(message)) {}
+
+    void addContext(const std::string& context) { m_what += "\n" + context; }
+
+    virtual const char* what() const noexcept override { return m_what.c_str(); }
+
+private:
+    std::string m_what;
+};
--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,9 +1,25 @@
 // Copyright (c) 2025 Pyarelal Knowles, MIT License
 
+#include "vulkan/vulkan_core.h"
 #include <gtest/gtest.h>
+#include <vko/dynamic_library.hpp>
+#include <vko/functions.hpp>
 #include <vko/handles.hpp>
 
 TEST(Integration, Init)
 {
+    vko::VulkanLibrary library;
+    vko::GlobalCommands globalCommands(library.loader());
+    VkInstanceCreateInfo instanceCreateInfo{
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .pApplicationInfo = nullptr,
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = nullptr,
+        .enabledExtensionCount = 0,
+        .ppEnabledExtensionNames = nullptr,
+    };
+    vko::Instance instance(vko::InstanceHandle(globalCommands, instanceCreateInfo), library.loader());
     EXPECT_EQ(1, 2);
 }
