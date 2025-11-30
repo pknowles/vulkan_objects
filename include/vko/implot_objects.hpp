d75df540b1db5f7d99d25de176d1b5b742bd477f
pknowles <pknowles@users.noreply.github.com>
2025-11-29 19:40:52 -0800

    wip

---
+++ include/vko/implot_objects.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2025 Pyarelal Knowles, MIT License
+#pragma once
+
+#include <implot.h>
+
+namespace vko {
+namespace implot {
+
+// RAII wrapper for ImPlot context
+class Context {
+public:
+    Context() {
+        ImPlot::CreateContext();
+    }
+    ~Context() {
+        ImPlot::DestroyContext();
+    }
+    Context(const Context&) = delete;
+    Context& operator=(const Context&) = delete;
+};
+
+} // namespace implot
+} // namespace vko
+
